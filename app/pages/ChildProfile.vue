cd4405f49648bfd698346678f1d9ad9e7c1709b5
Yana1606 <istominayana20@gmail.com>
2026-04-06 17:14:06 +0600

    fix: child_profile

---
+++ app/pages/ChildProfile.vue
@@ -0,0 +1,116 @@
+<template>
+  <Page actionBarHidden="true" class="page">
+    <ScrollView>
+      <StackLayout class="mainContent">
+        <Label text="Имя ребенка" class="titleLabel" />
+        
+        <TextField v-model="childName" hint="Имя" class="nameInput" />
+        
+        <Label text="Пол" class="titleLabel" />
+        
+        <GridLayout columns="auto, auto" class="genderRow" horizontalAlignment="center">
+          <StackLayout col="0" @tap="selectGender('boy')" class="genderItem">
+            <Image :src="selectedGender === 'boy' ? 'res://boy_selected' : 'res://boy'" 
+                   width="90" height="90" stretch="aspectFit" />
+          </StackLayout>
+          <StackLayout col="1" @tap="selectGender('girl')" class="genderItem">
+            <Image :src="selectedGender === 'girl' ? 'res://girl_selected' : 'res://girl'" 
+                   width="90" height="90" stretch="aspectFit" />
+          </StackLayout>
+        </GridLayout>
+        
+        <!-- Кнопка "Продолжить" -->
+        <StackLayout class="buttonWrapper" @tap="continuePressed">
+          <StackLayout class="buttonMain">
+            <Label text="Продолжить" class="buttonText" />
+          </StackLayout>
+        </StackLayout>
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      childName: '',
+      selectedGender: ''
+    };
+  },
+  methods: {
+    selectGender(gender) {
+      this.selectedGender = gender;
+    },
+    continuePressed() {
+      console.log('Имя:', this.childName);
+      console.log('Пол:', this.selectedGender);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.page {
+  background-color: #FFFCE1;
+}
+
+.mainContent {
+  padding: 20;
+  margin-top: 60;
+  margin-bottom: 40;
+}
+
+.titleLabel {
+  font-size: 32;
+  font-weight: bold;
+  color: #3D0000;
+  text-align: center;
+  margin-bottom: 20;
+  margin-top: 20;
+}
+
+.nameInput {
+  background-color: white;
+  border-radius: 85;
+  border-width: 2;
+  border-color: #F68B3C;
+  padding: 20;
+  font-size: 20;
+  margin-left: 20;
+  margin-right: 20;
+}
+
+.genderRow {
+  margin-top: 40;
+  margin-bottom: 60;
+}
+
+.genderItem {
+  margin-left: 20;
+  margin-right: 20;
+}
+
+.buttonWrapper {
+  align-items: center;
+  margin-top: 20;
+  margin-bottom: 40;
+}
+
+.buttonMain {
+  width: 240;
+  height: 50;
+  background-color: #F68B3C;
+  border-radius: 85;
+  align-items: center;
+  justify-content: center;
+  elevation: 4;
+}
+
+.buttonText {
+  font-size: 18;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+}
+</style>
--- app/pages/Welcome.vue
+++ app/pages/Welcome.vue
@@ -1,17 +1,97 @@
 <template>
-  <Page actionBarHidden="true">
-    <GridLayout rows="*">
-
-      <Image src="res://forest" />
-
-      <StackLayout class="topText">
+  <Page actionBarHidden="true" class="page">
+    <GridLayout rows="auto, *, auto" columns="*">
+      <Image row="0" rowSpan="3" col="0" src="res://forest" stretch="aspectFit" horizontalAlignment="center" verticalAlignment="center" />
+      
+      <StackLayout row="0" col="0" class="topText">
         <Label text="Читаем с Кьюрио" class="title" />
       </StackLayout>
-
-      <StackLayout class="bottomArea">
-        <Button text="Войти через Google" class="googleBtn" />
+      
+      <StackLayout row="2" col="0" class="bottomArea">
+        <GridLayout class="googleBtn" columns="auto, *" @tap="googleLogin">
+          <Image src="res://icgoogle" class="googleIcon" />
+          <Label text="Войти через Google" class="googleBtnText" col="1" />
+        </GridLayout>
       </StackLayout>
-
     </GridLayout>
   </Page>
 </template>
+
+<script>
+export default {
+  methods: {
+    googleLogin() {
+      console.log("Нажата кнопка Google, переходим на ChildProfile");
+      // Убедитесь что путь к файлу правильный
+      this.$navigateTo(require("./ChildProfile.vue").default);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.page {
+  background-color: #ffffff; /* фон, если изображение не покрывает всю область */
+}
+
+.topText {
+  horizontal-align: center;
+  margin-top: 80;
+}
+
+.title {
+  font-size: 48;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  text-shadow: 2 2 6 rgba(0,0,0,0.6);
+}
+
+.bottomArea {
+  horizontal-align: center;
+  margin-bottom: 60; /* отступ от нижнего края */
+}
+
+.googleBtn {
+  background-color: white;
+  border-radius: 20;
+  padding: 12 20;
+  width: 340;
+  elevation: 2;
+  text-align: center;
+  vertical-align: center;
+}
+
+.googleIcon {
+  width: 24;
+  height: 24;
+  margin-right: 12;
+}
+
+.googleBtnText {
+  color: #444;
+  font-size: 18;
+  font-weight: normal;
+  text-align: center;
+}
+
+/* Адаптация для маленьких экранов */
+@media (max-width: 480) {
+  .title {
+    font-size: 36;
+    margin-top: 50;
+  }
+  .googleBtn {
+    width: 240;
+    padding: 10 16;
+  }
+  .googleIcon {
+    width: 20;
+    height: 20;
+    margin-right: 8;
+  }
+  .googleBtnText {
+    font-size: 16;
+  }
+}
+</style>
